4ff6ad3e98043eb072620aff01b502848f3c51ec
Matthew Sells <matthew.sells@proton.me>
2024-10-13 15:11:27 -0500

    move character on screen

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
 enum State {Idle, Walk}
 
 var state: State = State.Idle
+var speed: int = 200
+var target = position
 
 @onready var _sprite = $Sprite
 
@@ -14,6 +16,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _physics_process(_delta: float) -> void:
+	match state:
+		State.Walk:
+			velocity = position.direction_to(target) * speed
+			_sprite.flip_h = velocity.x < 0
+			
+			if position.distance_to(target) > 10:
+				move_and_slide()
+			else:
+				set_state(State.Idle)
+
+
+func _input(event):
+	if event.is_action_pressed(&"right_click"):
+		target = get_global_mouse_position()
+		set_state(State.Walk)
 
 
 func set_state(new_state: State) -> void:
